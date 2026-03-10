7f14b94e122fd16473fb744dc3cf673069fc9225
a2ngerer <alexander.angerer@outlook.de>
2026-03-10 13:32:19 +0100

    refactor(neurons): make LRC_Cell and LRC_AR_Cell inherit from BaseCell
    Removes duplicated state_size property and self.units assignment;
    delegates both to BaseCell. Exports BaseCell from the neurons package.

--- src/neurons/__init__.py
+++ src/neurons/__init__.py
@@ -1,4 +1,5 @@
+from .base_cell import BaseCell
 from .lrc_cell import LRC_Cell
 from .lrc_ar_cell import LRC_AR_Cell
 
-__all__ = ["LRC_Cell", "LRC_AR_Cell"]
+__all__ = ["BaseCell", "LRC_Cell", "LRC_AR_Cell"]
